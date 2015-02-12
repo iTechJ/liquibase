990ee977eb449ea5118ed08229bd016d3cb61681
AntonNekrasov <Anton.Nekrasov@wsa-212.icx.local>
2015-02-12 18:23:16 +0300

    stage_3 complete

--- stage_3/liquibase/src/test/java/com/itechart/liquibase/LiquibaseTest.java
+++ stage_3/liquibase/src/test/java/com/itechart/liquibase/LiquibaseTest.java
@@ -1,18 +1,23 @@
 package com.itechart.liquibase;
 
-import static org.junit.Assert.assertEquals;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-
+import liquibase.Contexts;
 import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
-import liquibase.resource.ClassLoaderResourceAccessor;
-import org.hsqldb.jdbc.JDBCDataSource;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.FileSystemResourceAccessor;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 /**
  * Tests for Liquibase schema creation.
@@ -21,24 +26,51 @@ import org.junit.runners.JUnit4;
  */
 public class LiquibaseTest {
 
-    @Test
-    public void thisAlwaysPasses() {
-        try {
-            JDBCDataSource dataSource = new JDBCDataSource();
-            dataSource.setDatabase("jdbc:hsqldb:file:/tmp/hsqldb/testdb");
-            dataSource.setUser("SA");
-            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(dataSource.getConnection()));
-            Liquibase liquibase = new Liquibase("user-db.xml", new ClassLoaderResourceAccessor(), database);
+    private static Connection conn;
 
-            liquibase.update(null);
-        } catch (Exception e) {
+    private static Liquibase liquibase;
 
-        }
+    private static Contexts ctx = new Contexts("test");
+
+    @BeforeClass
+    public static void createTestData() throws SQLException,
+            ClassNotFoundException, LiquibaseException {
+
+        Class.forName("org.h2.Driver");
+        conn = DriverManager.getConnection("jdbc:h2:~/liquitest;MODE=MySQL;IGNORECASE=TRUE;", "sa", "sa");
+
+        Database database = DatabaseFactory.getInstance()
+                .findCorrectDatabaseImplementation(new JdbcConnection(conn));
+
+        liquibase = new Liquibase("src/main/liquibase/test.changelog.xml",
+                new FileSystemResourceAccessor(), database);
+
+        liquibase.update(ctx);
 
     }
 
+    @AfterClass
+    public static void removeTestData() throws LiquibaseException, SQLException {
+        liquibase.rollback(1000, ctx);
+        conn.close();
+    }
+
     @Test
-    @Ignore
-    public void thisIsIgnored() {
+    public void testStudents() throws SQLException {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        int EXPECTED_NUMBER = 3;// is the number of records added in db.testdata.xml
+        try {
+
+            stmt = conn.prepareStatement("select count(*) as ST_NUM from STUDENT");
+            rs = stmt.executeQuery();
+            rs.next();
+            int numberOfUsers = rs.getInt("ST_NUM");
+            Assert.assertEquals(EXPECTED_NUMBER, numberOfUsers);
+
+        } finally {
+            rs.close();
+            stmt.close();
+        }
     }
 }
